46ae2e5c0880806cf44d0af7182c375e0893d99f
epigram <sangmin0418@gmail.com>
2022-01-21 15:19:28 +0900

    첫번째 이슈1브랜치 커밋

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,32 @@ console.log("2")
 console.log("3")
 console.log("4")
 console.log("5")
-function printTxt(txt){
-    conslole.log(txt);
+function printTxt(txt) {
+	conslole.log(txt);
 
 }
 printTxt("aa");
 
-function $(elm){
+function $(elm) {
 	switch (elm.charAt(0)) {
 		case "#":
 
 			return elm.getElementalById(elm.replace(0));
 			break;
-	
+
 		case ".":
 
 			return elm.getElementalByClassssss(elm.replace(0));//어쩌구 저쩌구..
 			break;
-	
+
+		case "":
+
+			return elm.getElementalName(elm);//어쩌구 저쩌구22..
+			break;
+
+
 		default:
-            return "어쩌구 저쩌구.."
+			return "어쩌구 저쩌구.."
 
 			break;
 	}
